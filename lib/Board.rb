53087983bc590ce1639c8903af0c04f4f273f750
nithindante <Rajkumar_nithin@yahoo.com>
2024-04-12 22:23:47 +0530

    Did all the necessary changes

--- lib/Board.rb
+++ lib/Board.rb
@@ -12,16 +12,16 @@ class Board
         p "the column is already filled"
         return "the column is already filled"
     elsif(arrays[1][index]!=nil)
-          p "kundi"
+
             arrays[0][index]=marker
     elsif(arrays[2][index]!=nil)
-        p "vali"
+
         arrays[1][index] = marker
     elsif(arrays[3][index]!=nil)
-      p "kushu"
+
       arrays[2][index] = marker
     else
-      p "fuck"
+
     arrays[3][index] = marker
     end
     return self
@@ -71,6 +71,7 @@ class Board
   end
 
   def check_who_is_the_winner
+
     combos_arr = check_winner
     combos_arr.each do |element|
       if element[0]==element[1]
@@ -87,9 +88,9 @@ end
 
 def check_result(result,player1,player2)
   if result == player1.marker
-    return "#{player1.name} is the winner"
+    return 1
   elsif result == player2.marker
-    return "#{player2.name} is the winner"
+    return 2
   end
 end
 
@@ -97,10 +98,9 @@ p "Enter the First player's name and marker"
 player1 = Players.new(gets.chomp, gets.chomp)
 p "Enter the Second player's name and marker"
 player2 = Players.new(gets.chomp, gets.chomp)
-
 c = Board.new
 
-for i in 0..19
+for n in 0..19
  p " Please enter the positon you would like to insert the first person marker"
  i = gets.chomp.to_i
  while (c.insert_values(player1.marker, i-1).to_s).eql?("the column is already filled")
@@ -108,15 +108,23 @@ for i in 0..19
   i = gets.chomp.to_i
   c.insert_values(player1.marker, i-1)
  end
+ p c
  result = c.check_who_is_the_winner
- check_result(result,player1,player2)
+ if check_result(result,player1,player2) == 1
+  p "Player 1 is the winner"
+  return
+ end
  p " Please enter the positon you would like to insert the second person marker"
  j = gets.chomp.to_i
- while (c.insert_values(player1.marker, j-1).to_s).eql?("the column is already filled")
+ while (c.insert_values(player2.marker, j-1).to_s).eql?("the column is already filled")
   p " Please enter the positon you would like to again insert the second person marker"
   j = gets.chomp.to_i
   c.insert_values(player1.marker, j-1)
  end
+ p c
  result = c.check_who_is_the_winner
- check_result(result,player1,player2)
+ if check_result(result,player1,player2) == 2
+  p "Player 2 is the winner"
+  return
+ end
 end
